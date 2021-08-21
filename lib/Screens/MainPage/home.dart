529b5725570bdac0c225acc9be23a5866716fff0
Fatima <f.kaouk@st.ul.edu.lb>
2021-08-21 21:42:01 +0300

    //added home screen

--- lib/Screens/MainPage/home.dart
+++ lib/Screens/MainPage/home.dart
@@ -16,10 +16,8 @@ class _HomeState extends State<Home> {
     return bodyContent;
   }
 
-
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
     getWeather();
   }
@@ -73,228 +71,238 @@ Widget get bodyContent {
   );
 }
 
-void getWeather() async{
-  String _cityName='Beirut';
+Future<String> getWeather() async {
+  String _cityName = 'Beirut';
   WeatherFactory wf = new WeatherFactory(API_KEY);
   weather = await wf.currentWeatherByCityName(_cityName);
-  print(weather);
+  return weather.toString();
 }
 
 class WeatherWidget extends StatefulWidget {
-
   @override
   _WeatherWidgetState createState() => _WeatherWidgetState();
 }
 
 class _WeatherWidgetState extends State<WeatherWidget> {
+
   @override
   Widget build(BuildContext context) {
-    Row city = Row(mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: <Widget>[
-        Column(children: <Widget>[
-          Text(
-            weather.areaName,
-            style: TextStyle(
-              fontSize: 34,
-              color: Colors.black,
-            ),
-          ),
-        ]),
-        Spacer(),
-        IconButton(
-          icon: Icon(Icons.refresh),
-          onPressed: (){
-            setState(() {
-              getWeather();
-            });
-          },
-        ),
-      ],
-    );
-    Row date = Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            Text(DateFormatter.date(weather.date),
-                style: TextStyle(
-                  fontSize: 12,
-                  color: Colors.black87,
-                )),
-          ],
-        ),
-      ],
-    );
-    Row temperature = Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            SizedBox(height: 40),
-            Text(weather.temperature.toString(),
-                style: TextStyle(
-                  fontSize: 60,
-                  color: Colors.black87,
-                ))
-          ],
-        ),
-      ],
-    );
-    Row description = Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        //TODO: place image of weather cond
-        //Icon(Icons.),
-        SizedBox(width: 9),
-        Text(weather.weatherDescription,
-            style: TextStyle(
-              fontSize: 19,
-              color: Colors.black87,
-            )),
-      ],
-    );
-    double maxWindSpeed = 25.0;
-    final wind = LinearProgressIndicator(
-      backgroundColor: Colors.white24,
-      value: weather.windSpeed / maxWindSpeed,
-    );
-    double maxPressure = 1058.0;
-    final pressurec = LinearProgressIndicator(
-      backgroundColor: Colors.white24,
-      value: weather.pressure / maxPressure,
-    );
-    final humidityc = LinearProgressIndicator(
-      backgroundColor: Colors.white24,
-      value: weather.humidity / 100.0,
-    );
-    Row other = Row(
-      mainAxisAlignment: MainAxisAlignment.start,
-      mainAxisSize: MainAxisSize.max,
-      children: <Widget>[
-        Column(
-          children: <Widget>[
-            SizedBox(height: 20),
-            Row(children: <Widget>[
-              Container(
-                margin: const EdgeInsets.all(8.0),
-                child: Column(
+    return FutureBuilder<String>(
+        future: getWeather(),
+        builder: (context, AsyncSnapshot<String> snapshot) {
+          if (snapshot.hasData) {
+            Row city = Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Column(children: <Widget>[
+                  Text(
+                    weather.areaName,
+                    style: TextStyle(
+                      fontSize: 34,
+                      color: Colors.black,
+                    ),
+                  ),
+                ]),
+                Spacer(),
+                IconButton(
+                  icon: Icon(Icons.refresh),
+                  onPressed: () {
+                    setState(() {
+                      getWeather();
+                    });
+                  },
+                ),
+              ],
+            );
+            Row date = Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
                   children: <Widget>[
-                    Text("Wind",
+                    Text(DateFormatter.date(weather.date),
                         style: TextStyle(
                           fontSize: 12,
                           color: Colors.black87,
                         )),
-                    SizedBox(height: 12),
-                    Text(weather.windSpeed.toString(),
+                  ],
+                ),
+              ],
+            );
+            Row temperature = Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 40),
+                    Text(weather.temperature.toString(),
                         style: TextStyle(
-                          fontSize: 22,
+                          fontSize: 60,
                           color: Colors.black87,
-                        )),
-                    SizedBox(height: 10),
-                    Text("m/s",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 7),
-                    SizedBox(
-                      height: 2,
-                      width: 80,
-                      child: wind,
+                        ))
+                  ],
+                ),
+              ],
+            );
+            Row description = Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                //TODO: place image of weather cond
+                SizedBox(width: 9),
+                Text(weather.weatherDescription,
+                    style: TextStyle(
+                      fontSize: 19,
+                      color: Colors.black87,
+                    )),
+              ],
+            );
+            double maxWindSpeed = 25.0;
+            final wind = LinearProgressIndicator(
+              backgroundColor: Colors.white24,
+              value: weather.windSpeed / maxWindSpeed,
+            );
+            double maxPressure = 1058.0;
+            final pressurec = LinearProgressIndicator(
+              backgroundColor: Colors.white24,
+              value: weather.pressure / maxPressure,
+            );
+            final humidityc = LinearProgressIndicator(
+              backgroundColor: Colors.white24,
+              value: weather.humidity / 100.0,
+            );
+            Row other = Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              mainAxisSize: MainAxisSize.max,
+              children: <Widget>[
+                Column(
+                  children: <Widget>[
+                    SizedBox(height: 20),
+                    Row(
+                      children: <Widget>[
+                        Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text("Wind",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 12),
+                              Text(weather.windSpeed.toString(),
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 10),
+                              Text("m/s",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 7),
+                              SizedBox(
+                                height: 2,
+                                width: 80,
+                                child: wind,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text("Pressure",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 12),
+                              Text(weather.pressure.toString(),
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 10),
+                              Text("hPa",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 7),
+                              SizedBox(
+                                height: 2,
+                                width: 80,
+                                child: pressurec,
+                              ),
+                            ],
+                          ),
+                        ),
+                        Container(
+                          margin: const EdgeInsets.all(8.0),
+                          child: Column(
+                            children: <Widget>[
+                              Text("Humidity",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 12),
+                              Text(weather.humidity.toString(),
+                                  style: TextStyle(
+                                    fontSize: 22,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 10),
+                              Text("%",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black87,
+                                  )),
+                              SizedBox(height: 7),
+                              SizedBox(
+                                height: 2,
+                                width: 80,
+                                child: humidityc,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ],
                 ),
-              ),
-              Container(
-                margin: const EdgeInsets.all(8.0),
+              ],
+            );
+            Container container = Container(
+              margin: EdgeInsets.all(0),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
                 child: Column(
-                  children: <Widget>[
-                    Text("Pressure",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 12),
-                    Text(weather.pressure.toString(),
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 10),
-                    Text("hPa",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 7),
-                    SizedBox(
-                      height: 2,
-                      width: 80,
-                      child: pressurec,
-                    ),
-                  ],
+                  children: <Widget>[city, date, temperature, description, other],
                 ),
               ),
-              Container(
-                margin: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: <Widget>[
-                    Text("Humidity",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 12),
-                    Text(weather.humidity.toString(),
-                        style: TextStyle(
-                          fontSize: 22,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 10),
-                    Text("%",
-                        style: TextStyle(
-                          fontSize: 12,
-                          color: Colors.black87,
-                        )),
-                    SizedBox(height: 7),
-                    SizedBox(
-                      height: 2,
-                      width: 80,
-                      child: humidityc,
-                    ),
-                  ],
-                ),
+            );
+            return container;
+          } else {
+            return SizedBox(
+              width: 400,
+              child: CircularProgressIndicator(
               ),
-            ],
-            ),
-          ],
-        ),
-      ],
+            );
+          }
+        }
     );
-    Container container = Container(
-      margin: EdgeInsets.all(0),
-      child: Padding(padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: <Widget>[
-            city,
-            date ,
-            temperature,
-            description,
-            other
-          ],
-        ),
-      ),
-    );
-    return container;
+
   }
 }
 
-Widget get topMessageWidget{
+Widget get topMessageWidget {
   return Text(
     'Hello username,\nit seems cold today!',
     style: TextStyle(
@@ -305,7 +313,7 @@ Widget get topMessageWidget{
   );
 }
 
-Widget get newsWidget{
+Widget get newsWidget {
   return TextButton.icon(
     icon: Icon(
       Icons.web,
@@ -315,12 +323,11 @@ Widget get newsWidget{
     style: ButtonStyle(
       elevation: MaterialStateProperty.all(12.0),
       shadowColor: MaterialStateProperty.all<Color>(Colors.black),
-      backgroundColor: MaterialStateProperty.all<Color>(
-          primaryColor),
+      backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
           RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
-          )),
+        borderRadius: BorderRadius.circular(30.0),
+      )),
     ),
     onPressed: () {},
     label: Text(
@@ -335,22 +342,19 @@ Widget get newsWidget{
   );
 }
 
-Widget get agriculturalCalenderWidget{
+Widget get agriculturalCalenderWidget {
   return TextButton(
     style: ButtonStyle(
         elevation: MaterialStateProperty.all(12.0),
-        shadowColor:
-        MaterialStateProperty.all<Color>(Colors.black),
-        backgroundColor: MaterialStateProperty.all<Color>(
-            primaryColor),
-        shape:
-        MaterialStateProperty.all<RoundedRectangleBorder>(
+        shadowColor: MaterialStateProperty.all<Color>(Colors.black),
+        backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
-            ))),
+          borderRadius: BorderRadius.circular(30.0),
+        ))),
     onPressed: () {},
     child: Column(
-      children:<Widget>[
+      children: <Widget>[
         Text(
           "Agricultural calender",
           textAlign: TextAlign.center,
@@ -372,32 +376,32 @@ Widget get agriculturalCalenderWidget{
   );
 }
 
-Widget get suggestionWidget{
+Widget get suggestionWidget {
   return TextButton(
     style: ButtonStyle(
         elevation: MaterialStateProperty.all(12.0),
-        shadowColor:
-        MaterialStateProperty.all<Color>(Colors.black),
-        backgroundColor: MaterialStateProperty.all<Color>(
-            primaryColor),
-        shape:
-        MaterialStateProperty.all<RoundedRectangleBorder>(
+        shadowColor: MaterialStateProperty.all<Color>(Colors.black),
+        backgroundColor: MaterialStateProperty.all<Color>(primaryColor),
+        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(30.0),
-            ))),
+          borderRadius: BorderRadius.circular(30.0),
+        ))),
     onPressed: () {},
     child: Column(
-      children: <Widget>[ Text(
-        "Need a suggestion?",
-        textAlign: TextAlign.center,
-        style: TextStyle(
-          fontFamily: 'DMSerifText',
-          color: Colors.white,
-          letterSpacing: 2.0,
-          fontSize: 20.0,
+      children: <Widget>[
+        Text(
+          "Need a suggestion?",
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontFamily: 'DMSerifText',
+            color: Colors.white,
+            letterSpacing: 2.0,
+            fontSize: 20.0,
+          ),
         ),
-      ),
-        SizedBox(height: 10.0,),
+        SizedBox(
+          height: 10.0,
+        ),
         Icon(
           Icons.lightbulb,
           size: 40.0,
